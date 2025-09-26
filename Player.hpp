59aefc4c5f12207cf6a8d4734e820b512ee8905a
MohammedDigitalSage <159501876+MohammedDigitalSage@users.noreply.github.com>
2025-09-25 20:13:17 -0400

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -6,31 +6,29 @@
 
 class Player {
 private:
-    Inventory inventory_;
-    std::string name_;
+    Inventory inventory_; // Player's inventory
+    std::string name_;    // Player's name
 
 public:
-    // constructor
+    /**
+    * @brief Constructs a Player with the given identifier.
+    * @param name A const. string reference to be the player name
+    * @param inventory A const ref. to an Inventory to copy
+    * and setup a Player's inventory_ member. If none provided,
+    * default value of a default constructed Inventory
+    */
     Player(const std::string& name, const Inventory& inventory = Inventory());
 
-    // copy constructor
-    Player(const Player& rhs);
+    // Big Five
+    Player(const Player& rhs);                // Copy constructor
+    Player(Player&& rhs) noexcept;            // Move constructor
+    Player& operator=(const Player& rhs);     // Copy assignment
+    Player& operator=(Player&& rhs) noexcept; // Move assignment
+    ~Player() = default;                      // Default destructor
 
-    // move constructor
-    Player(Player&& rhs) noexcept;
-
-    // copy assignment
-    Player& operator=(const Player& rhs);
-
-    // move assignment
-    Player& operator=(Player&& rhs) noexcept;
-
-    // destructor (default is fine)
-    ~Player() = default;
-
-    // getters
+    // Accessors
     std::string getName() const;
-    Inventory& getInventoryRef();
+    Inventory& getInventoryRef(); // returns reference (not const)
 };
 
 #endif
