b82d388bed669664863fcb73cc8cf9450f320b22
MohammedDigitalSage <159501876+MohammedDigitalSage@users.noreply.github.com>
2025-09-25 20:07:42 -0400

    Create Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,35 @@
+#include "Player.hpp"
+
+// constructor
+Player::Player(const std::string& name, const Inventory& inventory)
+    : name_(name), inventory_(inventory) {}
+
+// copy constructor
+Player::Player(const Player& rhs)
+    : name_(rhs.name_), inventory_(rhs.inventory_) {}
+
+// move constructor
+Player::Player(Player&& rhs) noexcept
+    : name_(std::move(rhs.name_)), inventory_(std::move(rhs.inventory_)) {}
+
+// copy assignment
+Player& Player::operator=(const Player& rhs) {
+    if (this != &rhs) {
+        name_ = rhs.name_;
+        inventory_ = rhs.inventory_;
+    }
+    return *this;
+}
+
+// move assignment
+Player& Player::operator=(Player&& rhs) noexcept {
+    if (this != &rhs) {
+        name_ = std::move(rhs.name_);
+        inventory_ = std::move(rhs.inventory_);
+    }
+    return *this;
+}
+
+// getters
+std::string Player::getName() const { return name_; }
+Inventory& Player::getInventoryRef() { return inventory_; }
