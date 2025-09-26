285918acecebbe83c082a85c6f87597bd12c39a9
cs-student785 <159501876+cs-student785@users.noreply.github.com>
2025-09-26 19:49:46 -0400

    Update Inventory.hpp

--- Inventory.hpp
+++ Inventory.hpp
@@ -3,16 +3,14 @@
 #include <vector>
 #include "Item.hpp"
 
-using ItemGrid = std::vector<std::vector<Item>>;
-
 class Inventory {
 public:
-    Inventory(const ItemGrid& items = ItemGrid(10, std::vector<Item>(10)), Item* equipped = nullptr);
+    Inventory(const std::vector<std::vector<Item>>& items = std::vector<std::vector<Item>>(10, std::vector<Item>(10)), Item* equipped = nullptr);
 
     Item* getEquipped() const;
     void equip(Item* itemToEquip);
     void discardEquipped();
-    ItemGrid getItems() const;
+    const std::vector<std::vector<Item>>& getItems() const;
     float getWeight() const;
     size_t getCount() const;
     Item at(size_t row, size_t col) const;
@@ -22,10 +20,10 @@ public:
     Inventory(Inventory&& rhs) noexcept;
     Inventory& operator=(const Inventory& rhs);
     Inventory& operator=(Inventory&& rhs) noexcept;
-    ~Inventory();
+    ~Inventory() noexcept;
 
 private:
-    ItemGrid inventory_grid_;
+    std::vector<std::vector<Item>> inventory_grid_;
     Item* equipped_;
     float weight_;
     size_t item_count_;
